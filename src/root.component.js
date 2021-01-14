a879fc0d705dab95522430d22f414c02a6cb2958
MeridithJunk <Meridith.a.junk@accenture.com>
2021-01-14 14:54:36 -0600

    testing out routing functionality

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,18 @@
-import React from "react";
 
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import yippiePage from "./yippie-page/yippie-page.component";
+import kiPage from "./ki-page/ki-page.component";
+
+
+export default function Root() {
+  return (
+    <div>
+    <h1>Hello to the Yippie Ki Yay page!</h1>
+    <BrowserRouter>
+      <Route path="/yippie" component={yippiePage} />
+      <Route path="/ki" component={kiPage} />
+    </BrowserRouter>
+      </div>
+  );
 }
---
+++ src/yippie-page/yippie-page.component.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function yippiePage() {
+  return <section>Yippie Ki Yay mother trucker! </section>;
+}
